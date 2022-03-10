3515809409eba7af61830eb2d82f61ac6a60842b
Özge Poyraz <71945096+ozgepoyraz@users.noreply.github.com>
2022-03-10 15:18:37 +0300

    commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const Post = require('./models/Post')
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const app = express()
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts,
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
+})
+
+app.get('/post', (req, res) => {
+  res.render('post')
+})
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body)
+  res.redirect('/')
+})
+
+app.listen(3000, () => {
+  console.log('3000 portu dinleniyor.')
+})
